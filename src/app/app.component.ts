2189d80547affee42e500f0538a14fba92cfba12
Ron <li.rongcheng@gmail.com>
2018-03-06 22:30:56 +1100

    successfully integrate ckeditor5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic/build/ckeditor';
+import * as uniqid from 'uniqid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public instanceCount = 1;
+  public ckeditorIds: string[] = [];
+
+  ngOnInit(): void {
+    this.ckeditorIds.push(uniqid());
+  }
+  createInstances(): void {
+    for (let i = 0; i < this.instanceCount; i++) {
+      ClassicEditor
+        .create( document.querySelector( `#my-ckeditor-instance-${this.ckeditorIds[i]}` ) )
+        .then( editor => {
+          // TODO: Do something with the editor
+        } )
+        .catch( error => {
+          console.error( error );
+        } );
+    }
+  }
+  createIds(): string[] {
+    this.ckeditorIds = [];
+    for (let i = 0; i < this.instanceCount; i++) {
+      this.ckeditorIds.push(uniqid());
+    }
+    return this.ckeditorIds;
+  }
 }
